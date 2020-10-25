1dae4d1526713b5817fe59d285132007b0e95b03
Aleksei Kukharchuk <65033359+Aleksei-Kukharchuk@users.noreply.github.com>
2020-10-25 11:03:06 +0200

    Исправил 6 задачу

--- task_06.js
+++ task_06.js
@@ -13,8 +13,19 @@ const input = document.querySelector("#validation-input");
 
 input.addEventListener("change", onInputCheck);
 
+/* 
 function onInputCheck(inputText) {
   inputText.currentTarget.value.length == input.dataset.length
-    ? input.classList.toggle("invalid")
-    : input.classList.add("valid", "invalid");
+    ? input.classList.add("valid")
+    : input.classList.remove("valid").add("invalid");
+} */
+
+function onInputCheck(inputText) {
+  if (inputText.currentTarget.value.length == input.dataset.length) {
+    input.classList.remove("invalid");
+    input.classList.add("valid")
+  } else { 
+    input.classList.remove("valid");
+    input.classList.add("invalid");
+  }
 }
